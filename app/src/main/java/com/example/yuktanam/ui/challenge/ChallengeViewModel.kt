1a8f79a40aff7eb7de4bb0756364b8f70ccd6b55
SanoMush <mustofahusni27@gmail.com>
2024-11-22 23:35:10 +0700

    challenge

--- app/src/main/java/com/example/yuktanam/ui/challenge/ChallengeViewModel.kt
+++ app/src/main/java/com/example/yuktanam/ui/challenge/ChallengeViewModel.kt
@@ -4,10 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+data class Challenge(val title: String, val description: String, val progress: Int, val reward: String)
+
 class ChallengeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is challenge Fragment"
+    private val _challenges = MutableLiveData<List<Challenge>>().apply {
+        value = listOf(
+            Challenge("Scan tanaman", "Melakukan pemindaian tanaman sebanyak 1 kali", 50, "25 Koin"),
+            Challenge("Tambahkan tanaman", "Tambahkan sebanyak 3 tanaman di menu beranda", 75, "75 Koin"),
+            Challenge("Tambahkan tanaman", "Tambahkan sebanyak 10 tanaman di menu beranda", 30, "300 Koin"),
+            Challenge("Tanaman favorite", "Tambahkan sebanyak 4 tanaman favorit Anda", 60, "60 Koin")
+        )
     }
-    val text: LiveData<String> = _text
+    val challenges: LiveData<List<Challenge>> = _challenges
 }

ed101bcc2aa32f86b15da655b0a5569f8f512a66
ainul <ainulmufidh@gmail.com>
2024-12-01 10:26:54 +0700

    add logic for galeri and cameraX

--- app/src/main/java/com/example/yuktanam/ui/addplants/AddPlantActivity.kt
+++ app/src/main/java/com/example/yuktanam/ui/addplants/AddPlantActivity.kt
@@ -1,21 +1,69 @@
 package com.example.yuktanam.ui.addplants
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import com.example.yuktanam.MainActivity
 import com.example.yuktanam.databinding.ActivityAddPlantBinding
+import com.example.yuktanam.ui.addplants.CameraActivity.Companion.CAMERAX_RESULT
 
 class AddPlantActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddPlantBinding
+
+    private var currentImageUri: Uri? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddPlantBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // BUTTON BACK TO BERANDA
         ButtonBack()
+
+        // PERMISION
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+
+        binding.btnGaleri.setOnClickListener { startGallery() }
+        binding.btnCamera.setOnClickListener { startCameraX() }
+//        binding.uploadButton.setOnClickListener { uploadImage() }
+    }
+
+    // Button Back Bawaan ponsel untuk Kembali ke Beranda
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this@AddPlantActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     // Button Back Kembali ke Beranda
@@ -26,5 +74,95 @@ class AddPlantActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    // GALERI
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        } else {
+            currentImageUri = null
+        }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERAX_RESULT) {
+            currentImageUri = it.data?.getStringExtra(CameraActivity.EXTRA_CAMERAX_IMAGE)?.toUri()
+            showImage()
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.thumbnailImage.setImageURI(it)
+        }
+    }
+
+//    private fun uploadImage() {
+//        currentImageUri?.let { uri ->
+//            val imageFile = uriToFile(uri, this).reduceFileImage()
+//            Log.d("Image File", "showImage: ${imageFile.path}")
+//            val description = "Ini adalah deksripsi gambar"
+//
+//            showLoading(true)
+//
+//            val requestBody = description.toRequestBody("text/plain".toMediaType())
+//            val requestImageFile = imageFile.asRequestBody("image/jpeg".toMediaType())
+//            val multipartBody = MultipartBody.Part.createFormData(
+//                "photo",
+//                imageFile.name,
+//                requestImageFile
+//            )
+//            lifecycleScope.launch {
+//                try {
+//                    val apiService = ApiConfig.getApiService()
+//                    val successResponse = apiService.uploadImage(multipartBody, requestBody)
+//                    showToast(successResponse.message)
+//                    showLoading(false)
+//                } catch (e: HttpException) {
+//                    val errorBody = e.response()?.errorBody()?.string()
+//                    val errorResponse = Gson().fromJson(errorBody, FileUploadResponse::class.java)
+//                    showToast(errorResponse.message)
+//                    showLoading(false)
+//                }
+//            }
+//        } ?: showToast(getString(R.string.empty_image_warning))
+//    }
+
+//    private fun showLoading(isLoading: Boolean) {
+//        binding.progressIndicator.visibility = if (isLoading) View.VISIBLE else View.GONE
+//    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
